4f333078c5c6072db4c4fb5f91d88ec0eb4d843c
CptZeru <zerulegend@gmail.com>
2020-05-10 23:50:27 +0700

    done learning redux

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-console.log("Hello World!");
+import store from './store';
+import {bugAdded, bugResolved} from './actions'
+// import * as actions from './actionTypes';
+
+const unsubcribe = store.subscribe(() => {
+    console.log("Store changed!", store.getState());
+});
+
+store.dispatch(bugAdded("Bug#1"));
+store.dispatch(bugResolved(1));
+
+
+console.log(store.getState());
